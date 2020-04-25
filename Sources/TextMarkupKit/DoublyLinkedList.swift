4d7e18637209298e73725230fbcde165e700ca05
Brian Dewey <bdewey@gmail.com>
2020-04-25 08:21:49 -0700

    WIP: "special sentinal node at end of list" implementation

--- Sources/TextMarkupKit/DoublyLinkedList.swift
+++ Sources/TextMarkupKit/DoublyLinkedList.swift
@@ -18,16 +18,19 @@
 import Foundation
 
 public final class DoublyLinkedList<Element>: ExpressibleByArrayLiteral {
-  private var listEnds: ListEnds?
+  private var head: Node
+  private let tail: Node
 
   public init() {
     count = 0
+    tail = Node()
+    head = tail
   }
 
   public convenience init<S: Sequence>(_ elements: S) where S.Element == Element {
     self.init()
     for element in elements {
-      insertAtTail(element)
+      insert(element, at: endIndex)
     }
   }
 
@@ -38,92 +41,113 @@ public final class DoublyLinkedList<Element>: ExpressibleByArrayLiteral {
   public private(set) var count: Int
 
   public var isEmpty: Bool {
-    return listEnds == nil
+    count == 0
   }
 
   public var last: Element? {
-    return listEnds?.tail.payload
+    tail.previous?.payload
   }
 
-  public func insertAtHead(_ element: Element) {
-    count += 1
+  public func insert(_ element: Element, at index: Index) {
     let node = Node(element)
-    if var listEnds = listEnds {
-      listEnds.head.backwardLink = node
-      node.forwardLink = listEnds.head
-      listEnds.head = node
-      node.backwardLink = nil
-      self.listEnds = listEnds
-    } else {
-      self.listEnds = ListEnds(head: node, tail: node)
-    }
-  }
-
-  public func insertAtTail(_ element: Element) {
+    index.node.previous?.next = node
+    node.previous = index.node.previous
+    node.next = index.node
+    index.node.previous = node
     count += 1
-    let node = Node(element)
-    if var listEnds = listEnds {
-      listEnds.tail.forwardLink = node
-      node.backwardLink = listEnds.tail
-      listEnds.tail = node
-      node.forwardLink = nil
-      self.listEnds = listEnds
-    } else {
-      self.listEnds = ListEnds(head: node, tail: node)
+    if index.node === head {
+      head = node
     }
   }
 
-  public func mergeAtTail(_ other: DoublyLinkedList<Element>) {
-    count += other.count
-    other.count = count
-    switch (listEnds, other.listEnds) {
-    case (.some(let listEnds), .some(let otherListEnds)):
-      listEnds.tail.forwardLink = otherListEnds.head
-      otherListEnds.head.backwardLink = listEnds.tail
-      let newListEnds = ListEnds(head: listEnds.head, tail: otherListEnds.tail)
-      self.listEnds = newListEnds
-      other.listEnds = newListEnds
-    case (.none, .some(let otherListEnds)):
-      listEnds = otherListEnds
-    case (.some(let listEnds), .none):
-      other.listEnds = listEnds
-    case (.none, .none):
-      break
+//  public func fuse(_ other: DoublyLinkedList<Element>) -> Index {
+//    let originalCount = count
+//    count += other.count
+//    other.count = count
+//    switch (listEnds, other.listEnds) {
+//    case (.some(let listEnds), .some(let otherListEnds)):
+//      let fuseIndex = Index(ordinal: originalCount - 1, node: listEnds.tail)
+//      listEnds.tail.forwardLink = otherListEnds.head
+//      otherListEnds.head.backwardLink = listEnds.tail
+//      let newListEnds = ListEnds(head: listEnds.head, tail: otherListEnds.tail)
+//      self.listEnds = newListEnds
+//      other.listEnds = newListEnds
+//      return fuseIndex
+//    case (.none, .some(let otherListEnds)):
+//      listEnds = otherListEnds
+//      return Index(ordinal: count - 1, node: otherListEnds.tail)
+//    case (.some(let listEnds), .none):
+//      other.listEnds = listEnds
+//      return Index(ordinal: count - 1, node: listEnds.tail)
+//    case (.none, .none):
+//      return endIndex
+//    }
+//  }
+
+  @discardableResult
+  public func remove(at index: Index) -> Element {
+    precondition(index != endIndex)
+    let node = index.node
+    count -= 1
+    node.previous?.next = node.next
+    node.next?.previous = node.previous
+    if node === head {
+      // Only the end index has a nil forwardLink
+      head = node.next!
     }
+    return node.payload!
   }
 }
 
-extension DoublyLinkedList: Sequence {
-  public struct Iterator: IteratorProtocol {
-    fileprivate var nextNode: Node?
+extension DoublyLinkedList: BidirectionalCollection {
+  public struct Index: Comparable {
+    fileprivate let ordinal: Int
+    fileprivate let node: Node
 
-    public mutating func next() -> Element? {
-      let nextElement = nextNode?.payload
-      nextNode = nextNode?.forwardLink
-      return nextElement
+    public static func < (lhs: DoublyLinkedList.Index, rhs: DoublyLinkedList.Index) -> Bool {
+      return lhs.node !== rhs.node && lhs.ordinal < rhs.ordinal
+    }
+
+    public static func == (lhs: DoublyLinkedList.Index, rhs: DoublyLinkedList.Index) -> Bool {
+      return lhs.node === rhs.node
     }
   }
 
-  public func makeIterator() -> Iterator {
-    Iterator(nextNode: listEnds?.head)
+  public var startIndex: Index {
+    Index(ordinal: 0, node: head)
+  }
+
+  public var endIndex: Index {
+    Index(ordinal: count, node: tail)
+  }
+
+  public subscript(position: Index) -> Element {
+    // node is non-nil for everything except `endIndex`. It's an error to subscript the end index,
+    // so crashing is the right thing.
+    position.node.payload!
+  }
+
+  public func index(after i: Index) -> Index {
+    Index(ordinal: i.ordinal + 1, node: i.node.next!)
+  }
+
+  public func index(before i: Index) -> Index {
+    precondition(i.ordinal > 0)
+    return Index(ordinal: i.ordinal - 1, node: i.node.previous!)
   }
 }
 
 // MARK: - Private
 
 private extension DoublyLinkedList {
-  struct ListEnds {
-    var head: Node
-    var tail: Node
-  }
-
-  class Node {
-    init(_ payload: Element) {
+  final class Node {
+    init(_ payload: Element? = nil) {
       self.payload = payload
     }
 
-    let payload: Element
-    var forwardLink: Node?
-    unowned var backwardLink: Node?
+    let payload: Element?
+    var isSentinel: Bool { payload == nil }
+    var next: Node?
+    unowned var previous: Node?
   }
 }
--- Tests/TextMarkupKitTests/DoublyLinkedListTests.swift
+++ Tests/TextMarkupKitTests/DoublyLinkedListTests.swift
@@ -24,7 +24,7 @@ final class DoublyLinkedListTests: XCTestCase {
     let values = [1, 2, 3, 4, 5]
     let list = DoublyLinkedList<Int>()
     for value in values {
-      list.insertAtTail(value)
+      list.insert(value, at: list.endIndex)
     }
     XCTAssertEqual(values, list.map { $0 })
   }
@@ -33,17 +33,39 @@ final class DoublyLinkedListTests: XCTestCase {
     let values = [1, 2, 3, 4, 5]
     let list = DoublyLinkedList<Int>()
     for value in values {
-      list.insertAtHead(value)
+      list.insert(value, at: list.startIndex)
     }
     XCTAssertEqual(values.reversed(), list.map { $0 })
   }
 
-  func testMergeLists() {
-    let list1: DoublyLinkedList<Int> = [1, 2, 3, 4, 5]
-    let list2: DoublyLinkedList<Int> = [6, 7, 8, 9, 0]
+//  func testMergeLists() {
+//    let list1: DoublyLinkedList<Int> = [1, 2, 3, 4, 5]
+//    let list2: DoublyLinkedList<Int> = [6, 7, 8, 9, 0]
+//
+//    let fusePoint = list1.fuse(list2)
+//    XCTAssertEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], list1.map { $0 })
+//    XCTAssertEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], list2.map { $0 })
+//    XCTAssertEqual([5, 6, 7, 8, 9, 0], list1[fusePoint ..< list1.endIndex].map { $0 })
+//    list1.remove(at: fusePoint)
+//    XCTAssertEqual([1, 2, 3, 4, 6, 7, 8, 9, 0], list1.map { $0 })
+//  }
 
-    list1.mergeAtTail(list2)
-    XCTAssertEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], list1.map { $0 })
-    XCTAssertEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], list2.map { $0 })
+  func testRemoveOnlyNode() {
+    let list: DoublyLinkedList<Int> = [312]
+    list.remove(at: list.startIndex)
+    XCTAssertTrue(list.isEmpty)
+  }
+
+  func testRemoveFirstItem() {
+    let list: DoublyLinkedList<Int> = [1, 2]
+    list.remove(at: list.startIndex)
+    XCTAssertEqual([2], list.map { $0 })
+  }
+
+  func testRemoveLastItem() {
+    let list: DoublyLinkedList<Int> = [1, 2]
+    let lastValidIndex = list.index(before: list.endIndex)
+    list.remove(at: lastValidIndex)
+    XCTAssertEqual([1], list.map { $0 })
   }
 }
